999a27a97a59ac3aa1dd3aaee2e1ba3dd43042bc
Tom Willemse <tom@ryuslash.org>
2015-06-22 16:38:50 +0200

    Use the opengraph API to get facebook shares

--- lib/social_shares/facebook.rb
+++ lib/social_shares/facebook.rb
@@ -1,17 +1,10 @@
 module SocialShares
   class Facebook < Base
-    URL = 'http://api.ak.facebook.com/restserver.php'
+    URL = 'https://graph.facebook.com/'
 
     def shares!
-      response = RestClient.get(URL, {
-        :params => {
-          :urls => checked_url,
-          :v => '1.0',
-          :method => 'links.getStats',
-          :format => 'json'
-        }
-      })
-      JSON.parse(response)[0]['share_count'] || 0
+      response = RestClient.get(URL, params: { id: checked_url })
+      JSON.parse(response)['shares'] || 0
     end
   end
 end
